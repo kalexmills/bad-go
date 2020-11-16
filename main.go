e694c21a1193a6d982f24744ed1aaf95b8179f8f
K. Alex Mills <k.alex.mills@gmail.com>
2020-11-16 08:55:27 -0600

    feat: f1rstp0st

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"	"runtime"
+	"sync"
+)
+
+
+func main() {
+
+}
+
+func test1() {
+	var out [][]int
+	for _, i := range [][1]int{{1}, {2}, {3}} {
+		out = append(out, i[:])
+	}
+	fmt.Println("Values:", out)
+}
+
+func test2() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 1; j++ {
+			wg.Add(1)
+			go func() {
+				fmt.Printf("%d ", i)
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+}
+
+func test3() {
+	vs := []int{1,2,3,4}
+	for _, v := range vs {
+		go func() {
+			fmt.Println(v)
+		}()
+	}
+	time.Sleep(1 *time.Second)
+}
